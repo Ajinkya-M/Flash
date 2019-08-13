f41453796cec83168bd26e018806589c500da2c1
Ajinkya Mankar <asanjaymanka@vmware.com>
2019-08-13 15:01:31 +0530

    First commit

---
+++ reservations/src/main/java/com/frankmoley/ReservationsApplicatino.java
@@ -0,0 +1,11 @@
+package com.frankmoley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplicatino {
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsApplicatino.class, args);
+	}
+}
